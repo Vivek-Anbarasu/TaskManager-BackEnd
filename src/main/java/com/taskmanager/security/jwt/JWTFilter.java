faa9fa3d959d12517196a5fff9a3360d4048bd71
Vivek <vivek.sixdee@gmail.com>
2026-01-25 16:34:29 +0530

    JWT changes

--- src/main/java/com/taskmanager/security/jwt/JWTFilter.java
+++ src/main/java/com/taskmanager/security/jwt/JWTFilter.java
@@ -42,7 +42,7 @@ public class JWTFilter extends OncePerRequestFilter {
         if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(email);
             if (jwtService.validateToken(email, userDetails.getUsername(),jwtToken)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             } else {
